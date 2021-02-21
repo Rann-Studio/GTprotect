a848b6905a78ee157a025644922c3ef9b8670ec8
exorcisT-Official <63898883+exorcisT-Official@users.noreply.github.com>
2021-02-21 20:43:46 +0700

    Add files via upload

---
+++ GTprotect.bat
@@ -0,0 +1,16 @@
+ÿþ
+@echo off
+
+TITLE Save.dat Protector By Rann
+COLOR a
+MODE CON: COLS=75 LINES=5
+
+ECHO.
+ECHO 	Growtopia Save.dat Protector By Rann.
+ECHO 	Version : Beta 1.0.7
+ECHO 	Don't Close This Program While Your Playing Growtopia
+GOTO Loop
+
+:Loop
+IF EXIST "C:\Users\ACER\AppData\Local\Growtopia\save.dat" DEL /F "C:\Users\ACER\AppData\Local\Growtopia\save.dat"
+GOTO Loop
